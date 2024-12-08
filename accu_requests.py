db1fda675956d2ec077ce1a8b609548ba38ccc61
IT4shnik <v.derevyanko@edu.centraluniversity.ru>
2024-12-09 02:03:29 +0300

    Almost done. Added index.html - template. Added error messages.

--- accu_requests.py
+++ accu_requests.py
@@ -1,18 +1,28 @@
 import requests
 
+
 from settings import *
 
 
-def bring_to_format(weather_data, loc_name, loc_country):
-    return {
-        "location": loc_name,   # Имя локации
-        "country": loc_country,   # Страна
-        "weather": weather_data[0]["WeatherText"],   # Погода в целом
-        "temperature": weather_data[0]["Temperature"]["Metric"]["Value"],  # Температура
-        "humidity": weather_data[0]["RelativeHumidity"],  # Влажность
-        "wind_speed": weather_data[0]["Wind"]["Speed"]["Metric"]["Value"],  # Скорость ветра
-        "wind_direction": weather_data[0]["Wind"]["Direction"]["Localized"],  # Направление ветра
-    }
+def bring_to_format(**kwargs):
+    if "error" not in kwargs:
+        loc_name = kwargs["loc_name"]
+        loc_country = kwargs["loc_country"]
+        weather_data = kwargs["weather_data"]
+        return {
+            "location": loc_name,   # Имя локации
+            "country": loc_country,   # Страна
+            "weather": weather_data[0]["IconPhrase"],   # Погода в целом
+            "temperature": f"{int(float((weather_data[0]["Temperature"]["Value"]) - 32) * 5 / 9)}",  # Температура в Цельсиях
+            "humidity": f"{weather_data[0]["RelativeHumidity"]}",  # Относительная влажность
+            "wind_speed": f"{int(int(weather_data[0]["Wind"]["Speed"]["Value"]) * 1.60934)}",  # Скорость ветра в км/ч
+            "wind_direction": weather_data[0]["Wind"]["Direction"]["Localized"],  # Направление ветра
+            "error": ''
+        }
+    else:
+        return {
+            "error": kwargs["error"]
+        }
 
 
 class RequestHandler:
@@ -23,27 +33,38 @@ class RequestHandler:
         location_url = self.base_url + "/locations/v1/cities/geoposition/search"
         params = {
             "apikey": API_KEY,
-            "q": f"{lo},{la}"
+            "q": f"{lo},{la}",
+            "language": "ru"
         }
         response = requests.get(location_url, params=params)
 
         if response.status_code == 200:
             location_data = response.json()
             location_key = location_data["Key"]
-            return location_key, location_data["LocalizedName"], location_data["Country"]["LocalizedName"]
+            return location_key, location_data["LocalizedName"], location_data["Country"]["LocalizedName"], ''
         else:
-            print(f"Error: {response.status_code}")
-            exit()
+            return '', '', '', f"Ошибка при получении данных о локации: {response.status_code}, {response.text}"
 
     def get_weather_by_coords(self, lo, la):
-        loc_key, loc_name, loc_country = self.get_region_key(lo, la)
+        loc_key, loc_name, loc_country, loc_error = self.get_region_key(lo, la)
 
-        weather_url = self.base_url + f"/forecasts/v1/hourly/1hour/{loc_key}"
-        params = {
-            "apikey": API_KEY
-        }
-        response = requests.get(weather_url, params=params)
+        if not loc_key:
+            weather_url = self.base_url + f"/forecasts/v1/hourly/1hour/{loc_key}"
+            params = {
+                "apikey": API_KEY,
+                "details": "true",
+                "language": "ru"
+            }
+            response = requests.get(weather_url, params=params)
+        else:
+            response = ''
 
-        if response.status_code == 200:
+        if response.status_code == 200 and not loc_error:
             weather_data = response.json()
-            return bring_to_format(weather_data, loc_name, loc_country)
+            return bring_to_format(weather_data=weather_data, loc_name=loc_name, loc_country=loc_country)
+        elif loc_error:
+            return bring_to_format(error=loc_error)
+        else:
+            return bring_to_format(
+                error=f"Ошибка при получении данных о погоде: {response.status_code}, {response.text}"
+            )
--- main.py
+++ main.py
@@ -10,30 +10,16 @@ def index():
         # Получаем координаты из формы
         start_lat = request.form.get("start_lat")
         start_lon = request.form.get("start_lon")
-        end_lat = request.form.get("end_lat")
-        end_lon = request.form.get("end_lon")
 
         req_handler = RequestHandler("https://dataservice.accuweather.com")
 
         # Получаем погоду для обеих точек
-        start_weather = req_handler.get_weather_by_coords(start_lat, start_lon)
-        end_weather = req_handler.get_weather_by_coords(end_lat, end_lon)
+        weather = req_handler.get_weather_by_coords(start_lat, start_lon)
 
         # Передаём данные в шаблон
-        return render_template("index.html", start_weather=start_weather, end_weather=end_weather)
+        return render_template("index.html", weather=weather)
 
     # GET-запрос: отображаем пустую форму
     return render_template("index.html")
-
-@app.route("/get_weather", methods=["POST"])
-def hello():
-    # Получим погоду в Долгопрудном (по заданию 1)
-    la, lo = 55.928910, 37.523294
-
-    req_handler = RequestHandler("https://dataservice.accuweather.com")
-    req_handler.get_weather_by_coords(la, lo)
-
-    return "Hi"
-
 
 app.run()
